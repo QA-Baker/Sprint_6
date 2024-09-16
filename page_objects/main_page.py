38fe41e53514113823dbc6ff1da90d2a7ef0399c
Konstantin Popov <popov.workemail@gmail.com>
2024-09-16 16:15:18 +0100

    do not create a new class attribute

--- page_objects/main_page.py
+++ page_objects/main_page.py
@@ -5,28 +5,27 @@ from locators.main_page_locators import MainPageLocators
 class MainPageMethods(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
-        self.locators = MainPageLocators()
 
     def click_price_and_payment(self):
-        self.click_element(self.locators.price_and_payment)
+        self.click_element(MainPageLocators.price_and_payment)
 
     def click_multiple_scooters(self):
-        self.click_element(self.locators.multiple_scooters)
+        self.click_element(MainPageLocators.multiple_scooters)
 
     def click_rental_time_calculation(self):
-        self.click_element(self.locators.rental_time_calculation)
+        self.click_element(MainPageLocators.rental_time_calculation)
 
     def click_order_today(self):
-        self.click_element(self.locators.order_today)
+        self.click_element(MainPageLocators.order_today)
 
     def click_extend_or_return(self):
-        self.click_element(self.locators.extend_or_return)
+        self.click_element(MainPageLocators.extend_or_return)
 
     def click_charger_included(self):
-        self.click_element(self.locators.charger_included)
+        self.click_element(MainPageLocators.charger_included)
 
     def click_cancel_order(self):
-        self.click_element(self.locators.cancel_order)
+        self.click_element(MainPageLocators.cancel_order)
 
     def click_outside_mkad(self):
-        self.click_element(self.locators.outside_mkad)
+        self.click_element(MainPageLocators.outside_mkad)
