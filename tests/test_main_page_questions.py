5b9dc76a681699e547c1813762ed87ab33df47a0
Konstantin Popov <popov.workemail@gmail.com>
2024-09-18 14:44:20 +0100

    remove unused methods

--- tests/test_main_page_questions.py
+++ tests/test_main_page_questions.py
@@ -1,7 +1,7 @@
 import pytest
 import allure
 from test_data import faq_test_data
-from page_objects.main_page import MainPageMethods
+from page_objects.base_page import BasePage
 
 
 @pytest.mark.usefixtures("main_page")
@@ -11,14 +11,14 @@ class TestMainPageQuestions:
     @allure.description("Тест проверяет, что при нажатии на вопрос в разделе FAQ раскрывается правильный текст.")
     @pytest.mark.parametrize("question_locator, answer_locator", faq_test_data)
     def test_faq_expand(self, main_page, question_locator, answer_locator):
-        main_page_methods = MainPageMethods(main_page)
+        base_page = BasePage(main_page)
 
         with allure.step(f"Клик по вопросу {question_locator}"):
-            main_page_methods.click_element_with_js(question_locator)
+            base_page.click_element_with_js(question_locator)
 
         with allure.step(f"Ожидание отображения ответа на вопрос {question_locator}"):
             # Используем find_element для ожидания отображения ответа
-            answer_element = main_page_methods.find_element(answer_locator)
+            answer_element = base_page.find_element(answer_locator)
 
         with allure.step(f"Проверка, что ответ {answer_locator} отображается"):
             assert answer_element.is_displayed(), f"Ответ на вопрос {question_locator} не отображается."
